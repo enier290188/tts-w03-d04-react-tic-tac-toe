3eed310249de4c57a60f88b03e5aecdc519cb0cd
Enier Ramos García <enier290188@gmail.com>
2020-11-08 15:01:31 -0500

    Congratulations! You now have a working tic-tac-toe game. And you’ve just learned the basics of React too. So you’re probably the real winner here.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Game from './game/Game';
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css"
+import "bootstrap/dist/js/bootstrap";
+import React from "react";
+import ReactDOM from "react-dom";
+import Board from "./game/components/Board";
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="rows">
+                    <div className="col-12">
+
+                        <div className="game">
+                            <div className="game-board">
+                                <Board/>
+                            </div>
+                            <div className="game-info">
+                                <div>{/* status */}</div>
+                                <ol>{/* TODO */}</ol>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 ReactDOM.render(
     <React.StrictMode>
         <Game/>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("game")
 );
